3e7fc001299a0e277724827a399f37b37f5c9f56
Carlos Gutierrez <cagutierrezr.17@icloud.com>
2023-09-09 16:38:23 -0300

    Tiempo Dojo
    Tarea Practica - Tiempo Dojo

---
+++ script.js
@@ -0,0 +1,12 @@
+var element = document.querySelector('.footer');
+function dltFooter() {
+    element.remove();
+}
+
+var alertTop = document.querySelector('.nav-link');
+console.log(alertTop);
+
+function alertTemp() {
+    alert('Loading weather report...')
+}
+
